866f153ac0000a9036e4b253a5203c824ea17551
Stef Meden <58486072+smeden-lod@users.noreply.github.com>
2023-12-20 11:45:28 +0100

    mini tuning

--- higher_order.py
+++ higher_order.py
@@ -1,29 +1,31 @@
 from chaine import Chaine
 
-def appliquer(self, f):
+def appliquer(ch, f):
     """
     f: int -> int une fonction qui prend en entrée un int et renvoie un int
-    self: Chaine
+    ch: Chaine
     renvoie la chaine construite à partir des f(x) pour chaque x dans l
     """
     pass
 
-def reduire(self, f, acc) -> int:
+def reduire(ch, f, acc) -> int:
     """
     f: (int, int) -> int en entrée un tuple de int renvoie un int
     acc: int
-    self: Chaine
+    ch: Chaine
     exemples: 
-    ch.reduire(lambda x, y: x+y, 0) calcule la somme des valeurs dans ch
-    ch.reduire(lambda x, y: x*y, 1) calcule le produit des valeurs dans ch
+    reduire(ch, lambda x, y: x+y, 0) calcule la somme des valeurs dans ch
+    reduire(ch, lambda x, y: x*y, 1) calcule le produit des valeurs dans ch
     """
     pass
 
-def filtrer(f, ch):
+def filtrer(ch, f):
     """
     f : int -> bool
-    self : Chaine
+    ch : Chaine
     renvoie la Chaine des valeurs x dans self telles que f(x) est True 
+    exemple:
+    filtrer(ch, lambda x: x<5) renvoie la chaine avec les valeurs dans ch qui sont <5 (dans le même ordre)
     """
     pass
 
